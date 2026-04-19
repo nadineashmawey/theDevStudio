f16ee0fd71873c49bd0c9c71bf6bbdbf2bcbdb23
zaina2403710-cloud <zaina2403710@miuegypt.edu.eg>
2026-04-19 16:57:32 -0700

    navigation bar jaaScript

---
+++ public/js/main-navbar.js
@@ -0,0 +1,58 @@
+// ===== GET ROLE =====
+function getRole() {
+  return localStorage.getItem("role"); 
+  // "instructor", "student", "admin", or null
+}
+
+// ===== COURSES =====
+function goCourses() {
+  window.location.href = "../pages/public/all-courses.html";
+}
+
+// ===== CHALLENGES =====
+function goChallenges() {
+  window.location.href = "../pages/public/coding-challenges.html";
+}
+
+// ===== DASHBOARD =====
+function goDashboard() {
+  const role = getRole();
+
+  if (role === "instructor") {
+    window.location.href = "../pages/instructor/dashboard.html";
+  } 
+  else if (role === "student") {
+    window.location.href = "../pages/student/dashboard.html";
+  } 
+  else if (role === "admin") {
+    window.location.href = "../pages/admin/dashboard.html";
+  } 
+  else {
+    // not logged in
+    window.location.href = "../pages/login.html";
+  }
+}
+
+// ===== PROFILE =====
+function goProfile() {
+  const role = getRole();
+
+  if (role === "instructor") {
+    window.location.href = "../pages/instructor/profile.html";
+  } 
+  else if (role === "student") {
+    window.location.href = "../pages/student/profile.html";
+  } 
+  else if (role === "admin") {
+    window.location.href = "../pages/admin/profile.html";
+  } 
+  else {
+    window.location.href = "../pages/login.html";
+  }
+}
+
+// ===== LOGOUT =====
+function logout() {
+  localStorage.removeItem("role");
+  window.location.href = "../index.html";
+}
