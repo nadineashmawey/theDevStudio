db226818b9697107406aa54ccb489026ff59e1c0
nadineashmawey <nadinetamer318@gmail.com>
2026-04-21 01:00:25 +0200

    fixed button links and javascript validation

--- public/js/login.js
+++ public/js/login.js
@@ -2,6 +2,7 @@ const roleButtons = document.querySelectorAll(".role-btn");
 const togglePassword = document.getElementById("togglePassword");
 const passwordInput = document.getElementById("password");
 const signinForm = document.getElementById("signinForm");
+const formMessage = document.getElementById("form-message");
 
 let selectedRole = "Student";
 
@@ -19,16 +20,44 @@ togglePassword.addEventListener("click", () => {
   togglePassword.textContent = hidden ? "🙈" : "👁";
 });
 
+
+
 signinForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const email = document.getElementById("email").value.trim();
   const password = passwordInput.value.trim();
 
+  // reset message
+  formMessage.textContent = "";
+
   if (!email || !password) {
-    alert("Please fill in all fields.");
+    formMessage.textContent = "Please fill in all fields.";
+    formMessage.style.color = "red";
     return;
   }
 
-  alert(`Signed in as ${selectedRole}`);
+  formMessage.textContent = "Signing in...";
+  formMessage.style.color = "#60A3A6"; 
+
+  // redirect after short delay
+  setTimeout(() => {
+    redirectUser();
+  }, 50);
 });
+
+function redirectUser() {
+  if (selectedRole === "Student") {
+    window.location.href = "/../../../public/pages/student/dashboard.html";
+  } 
+  else if (selectedRole === "Instructor") {
+    window.location.href = "/../../../public/pages/instructor/dashboard.html";
+  } 
+  else if (selectedRole === "Admin") {
+    window.location.href = "/../../../public/pages/admin/dashboard.html";
+  } 
+  else {
+    formMessage.textContent = "Please select a role.";
+    formMessage.style.color = "red";
+  }
+}
